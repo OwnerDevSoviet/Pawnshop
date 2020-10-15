26dd05e6dc4a80a11fd18f033ef35039f9899fb0
OwnerDevSoviet <70889124+OwnerDevSoviet@users.noreply.github.com>
2020-10-15 01:57:47 -0700

    Added 10 item support

--- config.lua
+++ config.lua
@@ -6,11 +6,14 @@ Config.Size2 = 1.0 --y
 Config.Size3 = 0.5 --z
 Config.Color = {r = 0, g = 250, b = 204}
 Config.PawnShop = {
-  vector3(-1237.94, -1506.34, 4.0)
-  --vector3(-1239.94, -1506.34, 4.0)
-
+  vector3(-1237.94, -1506.34, 4.0),
+  vector3(-1239.94, -1506.34, 4.0),
+  vector3(-1242.23, -1510.05, 4.0),
+  vector3(-1246.41, -1505.91, 4.0),
+  vector3(-1249.67, -1500.75, 4.0),
+  vector3(-1253.08, -1495.85, 4.0)
 }
-
+---Selling 
 Config.Item1label = 'Bread'
 Config.Item1 = 'bread'
 Config.ItemDirty1 = true
@@ -27,9 +30,32 @@ Config.Item4label = 'Water'
 Config.Item4 = 'water'
 Config.ItemDirty4 = false
 Config.ItemSellPrice4 = 9999
+Config.Item5label = 'Water'
+Config.Item5 = 'water'
+Config.ItemDirty5 = false
+Config.ItemSellPrice5 = 9999
+Config.Item6label = 'Water'
+Config.Item6 = 'water'
+Config.ItemDirty6 = false
+Config.ItemSellPrice6 = 9999
+Config.Item7label = 'Water'
+Config.Item7 = 'water'
+Config.ItemDirty7 = false
+Config.ItemSellPrice7 = 9999
+Config.Item8label = 'Water'
+Config.Item8 = 'water'
+Config.ItemDirty8 = false
+Config.ItemSellPrice8 = 9999
+Config.Item9label = 'Water'
+Config.Item9 = 'water'
+Config.ItemDirty9 = false
+Config.ItemSellPrice9 = 9999
+Config.Item10label = 'Water'
+Config.Item10 = 'water'
+Config.ItemDirty10 = false
+Config.ItemSellPrice10 = 9999
 
-
-
+--Buying
 Config.Itemlabel1 = 'Bread'-- Label it whatever you want :shrug:
 Config.ItemBuy1 = 'bread' -- spawncode
 Config.ItemBuyDirty1 = true -- true or false young padawon
@@ -50,3 +76,23 @@ Config.Itemlabel5 = 'Water'
 Config.ItemBuy5 = 'water'
 Config.ItemDirty5 = false
 Config.ItemBuyPrice5 = 1
+Config.Itemlabel6 = 'Bread'
+Config.ItemBuy6 = 'bread' 
+Config.ItemBuyDirty6 = true 
+Config.ItemBuyPrice6 = 1 
+Config.Itemlabel7 = 'Bread'
+Config.ItemBuy7 = 'bread'
+Config.ItemDirty7 = false
+Config.ItemBuyPrice7 = 1
+Config.Itemlabel8 = 'Water'
+Config.ItemBuy8 = 'water'
+Config.ItemDirty8 = false
+Config.ItemBuyPrice8 = 1
+Config.Itemlabel9 = 'Bread'
+Config.ItemBuy9 = 'bread'
+Config.ItemDirty9 = false
+Config.ItemBuyPrice9 = 11
+Config.Itemlabel10 = 'Water'
+Config.ItemBuy10 = 'water'
+Config.ItemDirty10 = false
+Config.ItemBuyPrice10 = 1
