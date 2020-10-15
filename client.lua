06b97b2bb990204e90d9d7df74d69930006f966a
OwnerDevSoviet <70889124+OwnerDevSoviet@users.noreply.github.com>
2020-10-15 01:56:54 -0700

    Added 10 Item support

--- client.lua
+++ client.lua
@@ -38,7 +38,7 @@ Citizen.CreateThread(function()
         SetBlipDisplay(blip, 6)
         SetBlipScale(blip, 0.9)
         BeginTextCommandSetBlipName("STRING")
-        AddTextComponentString("PawnShop")
+        AddTextComponentString("Pineapple PawnShop")
         EndTextCommandSetBlipName(blip)
     end
     while true do
@@ -57,13 +57,20 @@ Citizen.CreateThread(function()
         end
     end
 end)
+---------------------
 local isMenuOpen = false
 
 local options = {
-	{label = Config.Item1label.. ' - <span style="color:green;">$' .. Config.ItemSellPrice1 .. ' </span>', value = 'sell_item1'},
-	{label = Config.Item2label.. ' - <span style="color:green;">$' .. Config.ItemSellPrice2 .. ' </span>', value = 'sell_item2'},
-	{label = Config.Item3label.. ' - <span style="color:green;">$' .. Config.ItemSellPrice3 .. ' </span>', value = 'sell_item3'},
-	{label = Config.Item4label.. ' - <span style="color:green;">$' .. Config.ItemSellPrice4 .. ' </span>', value = 'sell_item4'},
+	{label = ' [1] ' .. Config.Item1label.. ' - <span style="color:green;">$' .. Config.ItemSellPrice1 .. ' </span>', value = 'sell_item1'},
+	{label = ' [2] ' .. Config.Item2label.. ' - <span style="color:green;">$' .. Config.ItemSellPrice2 .. ' </span>', value = 'sell_item2'},
+	{label = ' [3] ' .. Config.Item3label.. ' - <span style="color:green;">$' .. Config.ItemSellPrice3 .. ' </span>', value = 'sell_item3'},
+    {label = ' [4] ' .. Config.Item4label.. ' - <span style="color:green;">$' .. Config.ItemSellPrice4 .. ' </span>', value = 'sell_item4'},
+    {label = ' [5] ' .. Config.Item5label.. ' - <span style="color:green;">$' .. Config.ItemSellPrice5 .. ' </span>', value = 'sell_item5'},
+	--{label = ' [6] ' .. Config.Item6label.. ' - <span style="color:green;">$' .. Config.ItemSellPrice6 .. ' </span>', value = 'sell_item6'},
+	--{label = ' [7] ' .. Config.Item7label.. ' - <span style="color:green;">$' .. Config.ItemSellPrice7 .. ' </span>', value = 'sell_item7'},
+    --{label = ' [8] ' .. Config.Item8label.. ' - <span style="color:green;">$' .. Config.ItemSellPrice8 .. ' </span>', value = 'sell_item8'},
+   -- {label = ' [9] ' .. Config.Item9label.. ' - <span style="color:green;">$' .. Config.ItemSellPrice9 .. ' </span>', value = 'sell_item'},
+	--{label = ' [10] ' .. Config.Item10label.. ' - <span style="color:green;">$' .. Config.ItemSellPrice10 .. ' </span>', value = 'sell_item10'},
 	{label = "Trying to Buy?", value = 'open_secondary'},
 }
 
@@ -75,7 +82,7 @@ function OpenMenu()
         align    = "center",
         elements = options
     }, function(data, menu)
-        menu.close()
+       -- menu.close() --Uncomment if you want menu to close after each item selection
         isMenuOpen = false
 
          if data.current.value == 'sell_item1' then
@@ -93,8 +100,23 @@ function OpenMenu()
 		 if data.current.value == 'open_secondary' then
             OpenSecondMenu()
 		 end
+		 if data.current.value == 'sell_item6' then
+            SellItem6()
+		 end
+		 if data.current.value == 'sell_item7' then
+            SellItem7()
+         end
+         if data.current.value == 'sell_item8' then
+            SellItem8()
+         end
+         if data.current.value == 'sell_item9' then
+            SellItem9()
+         end
+		 if data.current.value == 'sell_item10' then
+            SellItem10()
+         end
     end,
-    function(data, menu) 
+    function(data, menu) -- Closes Menu
         menu.close()
         isMenuOpen = false
     end)
@@ -104,11 +126,17 @@ end
 local isMenu2Open = false
 
 local opt = {
-	{label = Config.Itemlabel1.. ' - <span style="color:green;">$' .. Config.ItemBuyPrice1 .. ' </span>', value = 'buy_item1'},
-	{label = Config.Itemlabel2.. ' - <span style="color:green;">$' .. Config.ItemBuyPrice2 .. ' </span>', value = 'buy_item2'},
-	{label = Config.Itemlabel3.. ' - <span style="color:green;">$' .. Config.ItemBuyPrice3 .. ' </span>', value = 'buy_item3'},
-	{label = Config.Itemlabel4.. ' - <span style="color:green;">$' .. Config.ItemBuyPrice4 .. ' </span>', value = 'buy_item4'},
-	{label = Config.Itemlabel5.. ' - <span style="color:green;">$' .. Config.ItemBuyPrice5 .. ' </span>', value = 'buy_item5'},
+	{label = ' [1] ' ..  Config.Itemlabel1.. ' - <span style="color:green;">$' .. Config.ItemBuyPrice1 .. ' </span>', value = 'buy_item1'},
+	{label = ' [2] ' ..   Config.Itemlabel2.. ' - <span style="color:green;">$' .. Config.ItemBuyPrice2 .. ' </span>', value = 'buy_item2'},
+	{label = ' [3] ' ..   Config.Itemlabel3.. ' - <span style="color:green;">$' .. Config.ItemBuyPrice3 .. ' </span>', value = 'buy_item3'},
+	{label = ' [4] ' ..   Config.Itemlabel4.. ' - <span style="color:green;">$' .. Config.ItemBuyPrice4 .. ' </span>', value = 'buy_item4'},
+    {label = ' [5] ' ..   Config.Itemlabel5.. ' - <span style="color:green;">$' .. Config.ItemBuyPrice5 .. ' </span>', value = 'buy_item5'},
+    --{label = ' [6] ' ..   Config.Itemlabel6.. ' - <span style="color:green;">$' .. Config.ItemBuyPrice6 .. ' </span>', value = 'buy_item6'},
+    --{label = ' [7] ' ..   Config.Itemlabel7.. ' - <span style="color:green;">$' .. Config.ItemBuyPrice7 .. ' </span>', value = 'buy_item7'},
+    --{label = ' [8] ' ..   Config.Itemlabel8.. ' - <span style="color:green;">$' .. Config.ItemBuyPrice8 .. ' </span>', value = 'buy_item8'},
+    --{label = ' [9] ' ..   Config.Itemlabel9.. ' - <span style="color:green;">$' .. Config.ItemBuyPrice9 .. ' </span>', value = 'buy_item9'},
+    --{label = ' [10] ' ..   Config.Itemlabel10.. ' - <span style="color:green;">$' .. Config.ItemBuyPrice10 .. ' </span>', value = 'buy_item10'},
+
 }
 
 function OpenSecondMenu()
@@ -119,7 +147,7 @@ function OpenSecondMenu()
         align    = "center",
         elements = opt
     }, function(data, menu)
-        menu.close()
+       -- menu.close() --Uncomment if you want menu to close after each item selection
         isMenu2Open = false
 
          if data.current.value == 'buy_item1' then
@@ -137,8 +165,26 @@ function OpenSecondMenu()
 		 if data.current.value == 'buy_item5' then
             BuyItem5()
          end
+         if data.current.value == 'buy_item6' then
+            BuyItem6()
+         end
+         if data.current.value == 'buy_item6' then
+            BuyItem7()
+         end
+         if data.current.value == 'buy_item7' then
+            BuyItem8()
+         end
+         if data.current.value == 'buy_item8' then
+            BuyItem5()
+         end
+         if data.current.value == 'buy_item9' then
+            BuyItem9()
+         end
+         if data.current.value == 'buy_item10' then
+            BuyItem10()
+         end
     end,
-    function(data, menu) 
+    function(data, menu) -- Closes Menu
         menu.close()
         isMenu2Open = false
     end)
@@ -161,6 +207,29 @@ function SellItem4()
 	TriggerServerEvent('pawnshop:sell4')
 end
 
+function SellItem5()
+	TriggerServerEvent('pawnshop:sell5')
+end
+
+function SellItem6()
+	TriggerServerEvent('pawnshop:sell6')
+end
+
+function SellItem7()
+	TriggerServerEvent('pawnshop:sell7')
+end
+
+function SellItem8()
+	TriggerServerEvent('pawnshop:sell8')
+end
+
+function SellItem9()
+	TriggerServerEvent('pawnshop:sell9')
+end
+
+function SellItem10()
+	TriggerServerEvent('pawnshop:sell10')
+end
 function BuyItem1()
 	TriggerServerEvent('pawnshop:buy1')
 end
@@ -179,4 +248,24 @@ end
 
 function BuyItem5()
 	TriggerServerEvent('pawnshop:buy5')
+end
+
+function BuyItem6()
+	TriggerServerEvent('pawnshop:buy6')
+end
+
+function BuyItem7()
+	TriggerServerEvent('pawnshop:buy7')
+end
+
+function BuyItem8()
+	TriggerServerEvent('pawnshop:buy8')
+end
+
+function BuyItem9()
+	TriggerServerEvent('pawnshop:buy9')
+end
+
+function BuyItem10()
+	TriggerServerEvent('pawnshop:buy10')
 end
